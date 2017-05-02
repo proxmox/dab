92146ce512c27b73e9d78d56249ad9e3ea8a410d
Dietmar Maurer <dietmar@proxmox.com>
2017-05-02 06:25:21 +0200

    add support for Ubuntu 17.04 (zesty)

--- DAB.pm
+++ DAB.pm
@@ -349,6 +349,8 @@ sub new {
 	$config->{ostype} = "ubuntu-16.04";
     } elsif ($suite eq 'yakkety') {
 	$config->{ostype} = "ubuntu-16.10";
+    } elsif ($suite eq 'zesty') {
+	$config->{ostype} = "ubuntu-17.04";
     } else {
 	die "unsupported debian suite '$suite'\n";
     }
@@ -382,7 +384,8 @@ sub new {
 	    push @{$config->{source}}, "http://security.debian.org SUITE/updates main contrib";
 	} elsif ($suite eq 'hardy' || $suite eq 'intrepid' || $suite eq 'jaunty' ||
 		 $suite eq 'xenial' || $suite eq 'wily' || $suite eq 'vivid' ||
-		 $suite eq 'trusty' || $suite eq 'precise' || $suite eq 'yakkety') {
+		 $suite eq 'trusty' || $suite eq 'precise' || $suite eq 'yakkety' ||
+		 $suite eq 'zesty') {
 	    my $comp = "main restricted universe multiverse";
 	    push @{$config->{source}}, "http://archive.ubuntu.com/ubuntu SUITE $comp"; 
 	    push @{$config->{source}}, "http://archive.ubuntu.com/ubuntu SUITE-updates $comp"; 
@@ -450,7 +453,7 @@ sub new {
     # we cannot exclude it (instead we disable udevd)
 
     if ($suite eq 'vivid' || $suite eq 'wily' || $suite eq 'xenial' ||
-	$suite eq 'yakkety') {
+	$suite eq 'yakkety' || $suite eq 'zesty') {
 	push @$incl, 'isc-dhcp-client';
 	push @$excl, qw(libmodule-build-perl);
     } elsif ($suite eq 'trusty') {
@@ -1130,7 +1133,7 @@ sub install_init_script {
     if ($suite eq 'etch' || $suite eq 'lenny') {
 	$self->ve_command ("update-rc.d $base start $prio $runlevel .");
     } elsif ($suite eq 'xenial' || $suite eq 'wily' || $suite eq 'vivid' ||
-	     $suite eq 'yakkety') {
+	     $suite eq 'yakkety' || $suite eq 'zesty') {
 	die "unable to install init script (system uses systemd)\n";
     } elsif ($suite eq 'trusty' || $suite eq 'precise') {
 	die "unable to install init script (system uses upstart)\n";
@@ -1249,7 +1252,8 @@ sub bootstrap {
     # avoid warnings about non-existent resolv.conf
     write_file ("", "$rootdir/etc/resolv.conf", 0644);
 
-    if ($suite eq 'yakkety' || $suite eq 'xenial' || $suite eq 'wily') {
+    if ($suite eq 'zesty' || $suite eq 'yakkety' ||
+	$suite eq 'xenial' || $suite eq 'wily') {
 	# no need to configure loopback device
     } else {
 	$data = "auto lo\niface lo inet loopback\n";
